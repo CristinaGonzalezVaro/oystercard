db68946302984b1efece461320838e49eedc72f6
Cristina <cristhinagv@gmail.com>
2022-01-24 16:22:43 +0000

    added test for top_up and top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,12 @@ describe Oystercard do
   it 'checks that the card has a balance' do
     expect(subject.balance).to eq 0
   end
+
+  describe '#top_up' do
+    it 'allows the card to be topped-up' do
+      expect{ subject.top_up(5) }.to change{ subject.balance }.by 5
+    end
+
+
+  end
 end
