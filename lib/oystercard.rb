cf8fae80b46eb66a9d6ae37544260746cb4b48b5
Cristina <cristhinagv@gmail.com>
2022-01-25 15:23:33 +0000

    added minimum fare tests and code

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,33 @@
 class Oystercard
-attr_reader :balance
-MAXIMUM_BALANCE = 90
+  attr_reader :balance
+  MAXIMUM_BALANCE = 90
+  MINIMUM_FARE = 1
 
   def initialize
     @balance = 0
+    @in_travel = false
   end
 
   def top_up(amount)
     @balance += amount
     raise "Maximum balance reached" if @balance > MAXIMUM_BALANCE
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def in_journey?
+    @in_travel 
+  end
+
+  def touch_in
+    raise "Insufficient funds." if @balance < MINIMUM_FARE
+    @in_travel = true
+  end
+
+  def touch_out
+    @in_travel = false
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,12 +7,24 @@ describe Oystercard do
 
   describe '#top_up' do
     it 'allows the card to be topped-up' do
-      expect{ subject.top_up(5) }.to change{ subject.balance }.by 5
+      expect { subject.top_up(5) }.to change{ subject.balance }.by 5
     end
 
     it 'returns an error if the new balance is over the limit' do
       expect { subject.top_up(100) }.to raise_error( "Maximum balance reached")
     end
+  end
 
+  it 'deducts fare from card' do
+    expect { subject.deduct(5) }.to change{ subject.balance }.by -5
+  end
+
+  it 'checks for in_journey? to be false' do
+    subject.touch_out
+    expect(subject.in_journey?).to eq false
+  end
+
+  it 'returns an error if card does not have sufficient balance' do
+    expect { subject.touch_in}.to raise_error("Insufficient funds.")
   end
 end
